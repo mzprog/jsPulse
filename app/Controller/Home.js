f3430947b558c4ff57e13e19d940a638da3e6472
Mohamad Zbib <mz32programming@gmail.com>
2021-08-06 23:23:40 +0300

    reading the public files

--- app/Controller/Home.js
+++ app/Controller/Home.js
@@ -1,10 +1,12 @@
 export default class Home{
     index = (data) => {
         return `
+			<link rel="stylesheet" href="/css/style.css">
         	<div>
           		<span><b>Some</b> HTML here</span>
 			</div>
 			<a href="/from">from</a>
+			<img src="/img/heart.png">
 		`;
     }
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import { createServer } from 'http'
 import router from './app/Config/Routes.js'
 
@@ -5,11 +6,22 @@ const port = 9090
 const ROOT_DIR = process.cwd()
 
 const requestListner = async function(req, res) {
-
-    res.setHeader('Content-type', 'text/html');
-    res.writeHead(200)
-    
-    res.end(await router.getRoute(req.method,req.url))
+    //if the request is a public file
+    let tmpFileName = ROOT_DIR + "/public" + req.url
+    console.log(tmpFileName)
+    if (req.url != "/" && fs.existsSync(tmpFileName)) {
+        res.writeHead(200)
+        fs.readFile(tmpFileName, function(err, data) {
+            console.log(err)
+            res.write(data);
+            return res.end();
+          });
+    }else{
+        res.setHeader('Content-type', 'text/html');
+        res.writeHead(200)
+        
+        res.end(await router.getRoute(req.method,req.url))
+    }
 }
 
 const server = createServer(requestListner)
