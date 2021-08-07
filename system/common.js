6c54fbae1c47c40dfcb6461e20dafa43c732542e
Mohamad Zbib <mz32programming@gmail.com>
2021-08-07 21:36:07 +0300

    add `ejs engine`

---
+++ system/common.js
@@ -0,0 +1,12 @@
+import ejs from 'ejs'
+
+const viewsDir = "app/View/"
+
+export function view(name, data){
+    let filename = viewsDir + name + ".ejs"
+    var resStr;
+     ejs.renderFile(filename, data, (err, str) =>{
+        resStr =  str
+    })
+    return resStr
+}
