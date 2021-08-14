6d8d0f21525c82af240a6e1857e63aa96ee61dcf
Mohamad Dia <mhmddia71@gmail.com>
2021-08-14 08:58:23 -0700

    add documantion to the `User` model

--- app/Model/User.js
+++ app/Model/User.js
@@ -2,6 +2,12 @@ import { Model } from "../../system/Model/Model";
 
 export class User extends Model{
 
+    /**
+     * This is how to use Model first use the constructor 
+     * and call the parent constructor and give it the 
+     * table name in the database..
+     * and the array or fields names without the id
+     */
     constructor(){
         super({
             table: "users",
