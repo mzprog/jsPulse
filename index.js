942c201b86fe997cba5d354a01b78009d95942bb
Mohamad Zbib <mz32programmer@gmail.com>
2021-08-15 12:36:05 +0300

    listen only to ipv4

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import router from './app/Config/Routes.js'
 const ROOT_DIR = process.cwd()
 
 const requestListner = async function(req, res) {
-    console.log(req.method, req.url);
+    console.log(req.method, req.url, req.connection.remoteAddress);
+
     //if the request is a public file
     let tmpFileName = ROOT_DIR + "/public" + req.url
     if (req.url != "/" && fs.existsSync(tmpFileName)) {
@@ -25,6 +26,7 @@ const requestListner = async function(req, res) {
 
 const server = createServer(requestListner)
 
-server.listen(env.PORT)
+server.listen(env.PORT, "0.0.0.0") // to listen for ipv4 only
+
 console.log("port number",env.PORT)
 console.log("working directory",ROOT_DIR)
