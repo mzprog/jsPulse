f81857b39e0a83433304bb78a56b26c196e73da1
Mohamad Zbib <mz32programmer@gmail.com>
2021-08-18 22:24:39 +0300

    make first page

--- app/Controller/Home.js
+++ app/Controller/Home.js
@@ -2,7 +2,6 @@ import { view } from "../../system/common.js";
 
 export default class Home{
     index = (data) => {
-        let date = new Date()
-		return view('index',{date: date})
+		return view('index')
     }
 }
--- system/common.js
+++ system/common.js
@@ -2,7 +2,7 @@ import ejs from 'ejs'
 
 const viewsDir = "app/View/"
 
-export function view(name, data){
+export function view(name, data={}){
     let filename = viewsDir + name + ".ejs"
     var resStr;
      ejs.renderFile(filename, data, (err, str) =>{
